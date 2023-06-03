c9f7a8a524e3a72755129f07b26651b486b1b68f
zanlucathiago <zanlucathiago@gmail.com>
2023-06-03 19:46:06 -0300

    Adicionado nodelinker

---
+++ frontend/src/question/responseInput/Dropdown.jsx
@@ -0,0 +1,35 @@
+import {
+  FormControl, MenuItem,
+  Select,
+  Typography,
+} from '@mui/material';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+function Dropdown({ children, onChange, value }) {
+  const handleChange = (e) => onChange({ options: [e.target.value] });
+  return (
+    <FormControl size="small">
+      <Select displayEmpty value={value.options[0] || ''} onChange={handleChange}>
+        <MenuItem value="">
+          <Typography color="rgba(0,0,0,.54)">Escolher</Typography>
+        </MenuItem>
+        {children}
+      </Select>
+    </FormControl>
+  );
+}
+
+Dropdown.propTypes = {
+  children: PropTypes.node,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.arrayOf(PropTypes.shape({
+    options: PropTypes.string,
+  })).isRequired,
+};
+
+Dropdown.defaultProps = {
+  children: null,
+};
+
+export default Dropdown;
