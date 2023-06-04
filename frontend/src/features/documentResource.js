58210ea833bd6fc2fe70c5f6f43a654e76b451ff
zanlucathiago <zanlucathiago@gmail.com>
2023-06-03 22:57:52 -0300

    Added proptypes and eslint

--- frontend/src/features/documentResource.js
+++ frontend/src/features/documentResource.js
@@ -2,14 +2,15 @@ import axios from 'axios';
 
 const API_URL = '/api/d/e/';
 
-const createDocument = (id, document) =>
-  axios.post(`${API_URL}${id}`, document).then((response) => response.data);
+const createDocument = (id, document) => axios.post(`${API_URL}${id}`, document).then((response) => response.data);
 
-const getDocumentList = (id) =>
-  axios.get(`${API_URL}${id}`).then((response) => response.data);
+const getDocumentList = (id) => axios.get(`${API_URL}${id}`).then((response) => response.data);
+
+const getDocument = (id, params) => axios.get(`${API_URL}${id}/view`, { params }).then((response) => response.data);
 
 const documentResource = {
   createDocument,
+  getDocument,
   getDocumentList,
 };
 
